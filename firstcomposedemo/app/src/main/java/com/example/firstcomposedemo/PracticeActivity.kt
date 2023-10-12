6da2cafa568f8c89c110ae0c523f5772096782d7
hujingting <brucehjt@gmail.com>
2023-10-12 15:31:03 +0800

    init project

--- firstcomposedemo/app/src/main/java/com/example/firstcomposedemo/PracticeActivity.kt
+++ firstcomposedemo/app/src/main/java/com/example/firstcomposedemo/PracticeActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -23,21 +27,24 @@ class PracticeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val name = mutableStateOf("hello")
+        /**
+         * kotlin 常用关键字 by，by 的后面是代理对象
+         */
+        var name by mutableStateOf("hello")
+        val text = mutableStateOf("1")
 
         setContent {
             Text(
-                text = name.value,
+                text = name,
                 modifier = Modifier
                     .background(Color.Cyan),
                 style = TextStyle(fontSize = 36.sp)
             )
-
         }
 
         lifecycleScope.launch {
             delay(3000)
-            name.value = "world"
+            name = "world"
         }
     }
 }
