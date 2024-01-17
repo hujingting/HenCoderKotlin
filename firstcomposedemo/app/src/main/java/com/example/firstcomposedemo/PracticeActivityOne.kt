3cf6c0c6298808e61a3c753a0d7ee3a937ee93fe
hujingting <brucehjt@gmail.com>
2024-01-17 16:47:14 +0800

    derivedStateOf() 和 remember() 的区别

--- firstcomposedemo/app/src/main/java/com/example/firstcomposedemo/PracticeActivityOne.kt
+++ firstcomposedemo/app/src/main/java/com/example/firstcomposedemo/PracticeActivityOne.kt
@@ -3,34 +3,51 @@ package com.example.firstcomposedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 class PracticeActivityOne : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //添加数据即可以实现赋值操作，自动刷新控件数据
-        val nums =  mutableStateListOf<Int>(1,2,3)
+        //使用 mutableStateListOf() 添加数据即可以实现赋值操作，自动刷新控件数据
+//        val nums =  mutableStateListOf<Int>(1,2,3)
+//
+//        setContent {
+//            Column {
+//
+//                Button(onClick = {
+//                    nums.add(nums.last() + 1)
+//                }) {
+//                    Text(text = "加1")
+//                }
+//
+//                for (num in nums) {
+//                    Text(text = "数字是$num")
+//                }
+//            }
+//        }
 
         setContent {
-            Column {
-                
-                Button(onClick = {
-                    nums.add(nums.last() + 1)
-                }) {
-                    Text(text = "加1")
-                }
-                
-                for (num in nums) {
-                    Text(text = "数字是$num")
-                }
-            }
+            var name by remember { mutableStateOf("bruce") }
+
+            val processName by remember { derivedStateOf { name.uppercase() } }
+//            val processName = remember(name) { name.uppercase() }
+            
+            Text(processName, modifier = Modifier.clickable {
+                name = "jingting"
+            })
+
         }
     }
 
